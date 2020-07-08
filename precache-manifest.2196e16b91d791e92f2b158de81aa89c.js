21d45bcc1452c6a4423d1850c5a9093471afaef6
Yann Mainier <yann.mainier@gmail.com>
2020-07-08 23:12:41 +0100

    Updates

---
+++ precache-manifest.2196e16b91d791e92f2b158de81aa89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d648eb21998d23ee8536fba4d323a2d9",
+    "url": "/pokentin-cards/index.html"
+  },
+  {
+    "revision": "e85a54c4f5de1b0ef6e7",
+    "url": "/pokentin-cards/static/css/main.6cb4b0c3.chunk.css"
+  },
+  {
+    "revision": "c6716de9214eeeec6e31",
+    "url": "/pokentin-cards/static/js/2.9f0b1984.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokentin-cards/static/js/2.9f0b1984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85a54c4f5de1b0ef6e7",
+    "url": "/pokentin-cards/static/js/main.ce9ae8e9.chunk.js"
+  },
+  {
+    "revision": "4d20e7b1e3d15296f8ca",
+    "url": "/pokentin-cards/static/js/runtime-main.3d8881fa.js"
+  }
+]);
